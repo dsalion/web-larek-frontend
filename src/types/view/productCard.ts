321a4aedc3051b093d1a8c16680106eca97706c4
Evgeniy Mamaev <dsalion44@gmail.com>
2024-10-14 13:17:46 +0300

    started

---
+++ src/types/view/productCard.ts
@@ -0,0 +1,20 @@
+export interface IProductCard {
+    id: number
+    name: string
+    description: string
+    price: number
+    image: string
+}
+
+export interface IProductCardSmall {
+    id: number
+    name: string
+    price: number
+    image: string
+}
+
+export interface IProductCardBasket {
+    id: number
+    name: string
+    price: number
+}
