463d2d8c89a31ad609c9ef7ad2c178721465f7be
Evgeniy Or <dsalion44@gmail.com>
2024-10-15 22:21:27 +0500

    added class basket + interfaces

---
+++ src/components/basket.ts
@@ -0,0 +1,17 @@
+import { IProductCardBasket } from "../types/view/productCard";
+
+export class Basket {
+    protected products: IProductCardBasket[] = [];
+
+    constructor() {};
+
+    addProduct(product: IProductCardBasket) {
+
+    }
+    deleteProduct(id: number) {}
+
+    getProducts() {}
+
+    clearProducts() {}
+
+    }
