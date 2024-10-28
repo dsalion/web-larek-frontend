999555740e6576d73350f4e420bae639b3701d4c
Evgeniy Or <dsalion44@gmail.com>
2024-10-28 21:44:37 +0500

    started orderdata

--- src/components/OrderData.ts
+++ src/components/OrderData.ts
@@ -1,5 +1,43 @@
+import { IEvents } from "./base/events"
 
 
 export class OrderData {
+    
+    protected _items: []
+    protected _payment: string
+    protected _email: string
+    protected _phone: string
+    protected _address: string
+    protected _total: number
+    protected events: IEvents
+
+        constructor(events: IEvents) {
+            this.events = events
+        }
+
+    set items(value: []) {
+        this._items = value
+    }
+
+    set payment(value: string) {
+        this._payment = value
+    }
+
+    set email(value: string) {
+        this._email = value
+    }
+
+    set phone(value: string) {
+        this._phone = value
+    }
+
+    set address(value: string){
+        this._address = value
+    }
+
+    set total(value: number) {
+        this._total = value
+    }
+
     
 }
